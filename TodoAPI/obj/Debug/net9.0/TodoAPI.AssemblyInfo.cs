b5b09fbc50323e04fdc828c32312abf1f96fdb90
Hungryvietdude <s2300809@edu.bc.fi>
2025-05-15 12:21:31 +0300

    TodoApi
    TodoApi tehtävän koodi

--- TodoAPI/obj/Debug/net9.0/TodoAPI.AssemblyInfo.cs
+++ TodoAPI/obj/Debug/net9.0/TodoAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ec06540c0b49bc0f8cfb2ddf2602ed6be753763")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
